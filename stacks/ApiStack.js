f88bf86ffce62b878d21dc8aa3b920aa1e536bd0
andnej <andre.prasetya@gmail.com>
2022-06-16 16:05:29 +0700

    added update

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -17,6 +17,8 @@ export function ApiStack({stack, app}) {
         routes: {
             "POST /notes": "functions/create.main",
             "GET /notes/{id}": "functions/get.main",
+            "GET /notes": "functions/list.main",
+            "PUT /notes/{id}": "functions/update.main",
         }
     });
 
